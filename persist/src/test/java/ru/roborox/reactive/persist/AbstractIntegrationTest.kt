8ffe5f322edf0220b9d1dafa34c5804acc45d8ba
Evgeny Nacu <evgeny.nacu@gmail.com>
2020-10-22 12:36:35 +0300

    fix(*): tests

--- persist/src/test/java/ru/roborox/reactive/persist/AbstractIntegrationTest.kt
+++ persist/src/test/java/ru/roborox/reactive/persist/AbstractIntegrationTest.kt
@@ -1,5 +1,6 @@
 package ru.roborox.reactive.persist
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.core.ReactiveMongoOperations
@@ -9,7 +10,7 @@ import ru.roborox.reactive.test.persist.MongoCleanup
 
 @MongoCleanup
 @SpringBootTest
-@ContextConfiguration(classes = [MockContext::class])
+@ContextConfiguration(classes = [MockContext::class, ObjectMapper::class])
 abstract class AbstractIntegrationTest {
     @Autowired
     protected lateinit var mongo: ReactiveMongoOperations
