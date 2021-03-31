1d19d2ceded23e7578119c748270e24508f3b22b
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-03-31 15:10:29 +0300

    fix(*): fix task configuration

--- task/src/main/java/ru/roborox/reactive/task/EnableRoboroxTask.java
+++ task/src/main/java/ru/roborox/reactive/task/EnableRoboroxTask.java
@@ -1,9 +1,6 @@
 package ru.roborox.reactive.task;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,8 +9,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@EnableReactiveMongoRepositories
-@EnableScheduling
-@ComponentScan
+@Import(TaskConfiguration.class)
 public @interface EnableRoboroxTask {
 }
